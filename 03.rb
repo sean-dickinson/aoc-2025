804a2cbf410729833037b32cdca0dd4df8096543
Sean Dickinson <sean@bythepixel.com>
2025-12-10 16:45:49 -0500

    feat: scaffold

---
+++ 03.rb
@@ -0,0 +1,11 @@
+module Day03
+  class << self
+    def part_one(input)
+      raise NotImplementedError
+    end
+
+    def part_two(input)
+      raise NotImplementedError
+    end
+  end
+end
---
+++ spec/03_spec.rb
@@ -0,0 +1,18 @@
+require "./03"
+RSpec.describe Day03 do
+  context "part 1" do
+    it "returns the correct answer for the example input" do
+      pending
+      input = File.readlines("spec/test_inputs/03.txt", chomp: true)
+      expect(Day03.part_one(input)).to eq 0 # TODO: replace with correct answer
+    end
+  end
+
+  context "part 2" do
+    it "returns the correct answer for the example input" do
+      pending
+      input = File.readlines("spec/test_inputs/03.txt", chomp: true)
+      expect(Day03.part_two(input)).to eq 0 # TODO: replace with correct answer
+    end
+  end
+end
