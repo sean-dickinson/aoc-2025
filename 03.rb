2c4117fe3d2113efc97311f8e8233cc5919bb6be
Sean Dickinson <sean@bythepixel.com>
2025-12-10 20:47:43 -0500

    feat: part 1

--- 03.rb
+++ 03.rb
@@ -1,7 +1,34 @@
 module Day03
+  Bank = Data.define(:batteries) do
+    class << self
+      def from(string)
+        new(string.chars.map(&:to_i))
+      end
+    end
+
+    def max_joltage
+      tens = max_tens
+      ones = maximize_ones(tens)
+      [tens, ones].join.to_i
+    end
+
+    private
+
+    def max_tens
+      batteries[0...-1].max
+    end
+
+    def maximize_ones(tens)
+      start = batteries.find_index(tens) + 1
+      batteries[start..].max
+    end
+  end
+
   class << self
     def part_one(input)
-      raise NotImplementedError
+      input.sum do |line|
+        Bank.from(line).max_joltage
+      end
     end
 
     def part_two(input)
--- spec/03_spec.rb
+++ spec/03_spec.rb
@@ -1,10 +1,37 @@
 require "./03"
 RSpec.describe Day03 do
+  describe "Bank" do
+    it "can be created from string" do
+      bank = Day03::Bank.from("987654321111111")
+      expect(bank).to be_a Day03::Bank
+    end
+
+    describe "#max_joltage" do
+      it "returns 98 for the first example" do
+        bank = Day03::Bank.from("987654321111111")
+        expect(bank.max_joltage).to eq 98
+      end
+
+      it "returns 89 for the second example" do
+        bank = Day03::Bank.from("811111111111119")
+        expect(bank.max_joltage).to eq 89
+      end
+
+      it "returns 78 for the third example" do
+        bank = Day03::Bank.from("234234234234278")
+        expect(bank.max_joltage).to eq 78
+      end
+      it "returns 92 for the fourth example" do
+        bank = Day03::Bank.from("818181911112111")
+        expect(bank.max_joltage).to eq 92
+      end
+    end
+  end
+
   context "part 1" do
     it "returns the correct answer for the example input" do
-      pending
       input = File.readlines("spec/test_inputs/03.txt", chomp: true)
-      expect(Day03.part_one(input)).to eq 0 # TODO: replace with correct answer
+      expect(Day03.part_one(input)).to eq 357
     end
   end
 
